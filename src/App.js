5bb82a659b86f977b481f2a80d7c17f2699205f0
rvineeth544 <rvineeth544@gmail.com>
2026-04-23 12:15:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = 'https://pedabumqcd.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
+  const apiUrl = 'https://ybsozl0nm2.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
